bd86489a11bb853a18e227d1d62800ee7ff01b0a
Mike Yacullo <mike.yacullo@gmail.com>
2015-01-14 09:09:52 -0500

    Created Option Scroller Prefab

---
+++ Assets/Scripts/OptionScrollerBehavior.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionScrollerBehavior : MonoBehaviour
+{
+
+	public string[] options;
+	public UnityEngine.UI.Text txtSelected;
+	
+	int index;
+
+	// Use this for initialization
+	void Start ()
+	{
+		SetOption (0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+	
+	public void ClickLeft ()
+	{
+		Debug.Log ("ClickLeft");
+		SetOption (index - 1);
+	}
+	
+	public void ClickRight ()
+	{
+		SetOption (index + 1);
+	}
+	
+	void SetOption (int newIndex)
+	{
+		Debug.Log ("SetOption " + newIndex);
+		if (newIndex > options.Length - 1) {
+			newIndex = 0;
+		}
+		if (newIndex < 0) {
+			newIndex = options.Length - 1;
+		}
+		index = newIndex;
+		txtSelected.text = options [index];
+	}
+}
